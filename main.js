bffb2df60ac9f7d11885bc32d29b5ba99cd2219f
olsenmatthew <z5187767@unsw.edu.au>
2018-12-17 13:33:43 -0500

    create account and login popup rendered in dom via vanilla js

--- main.js
+++ main.js
@@ -95,6 +95,7 @@ search_bar_icon.addEventListener("click", function search_for_video() {
 
 		if (new_link.includes("&list")) {
 			var playlist = new_link.split("&list")[1];
+			// TODO: save playlist to database
 			console.log("&list"+playlist);
 			new_link = new_link.split("&list")[0];
 		}
@@ -102,6 +103,7 @@ search_bar_icon.addEventListener("click", function search_for_video() {
 		var prefix = "https://www.youtube.com/embed/";
 		new_link = prefix + new_link;
 		console.log(new_link);
+		// TODO: save video to database along with playlist
 		video_iframe.setAttribute("src", new_link);
 		// document.location.reload(true);
 		console.log("new video\n");
@@ -112,7 +114,142 @@ search_bar_icon.addEventListener("click", function search_for_video() {
 });
 
 account_buttons_account.addEventListener("click", function show_hide_account_menu() {
+
 	console.log("account menu clicked\n");
+
+
+		if (document.getElementById("background_of_login_create")) {
+
+			var background_of_login_create = document.getElementById("background_of_login_create");
+
+			while(background_of_login_create.firstChild) {
+				background_of_login_create.removeChild(background_of_login_create.firstChild);
+			}
+
+			background_of_login_create.remove();
+
+		} else {
+
+			var background_of_login_create = document.createElement("div");
+			background_of_login_create.id = "background_of_login_create";
+			background_of_login_create.className = "login_container";
+
+			var div_outer = document.createElement("div");
+			div_outer.className = "row_login_create";
+			background_of_login_create.append(div_outer);
+
+			var div_inner_left = document.createElement("div");
+			div_inner_left.className = "col-md-6 login_form_left";
+			div_outer.append(div_inner_left);
+
+			// login form
+			var left_title = document.createElement("h3");
+			var left_title_text = document.createTextNode("Login");
+			left_title.append(left_title_text);
+			div_inner_left.append(left_title);
+
+			var div_form_group = document.createElement("div");
+			div_form_group.className = "form_group";
+			div_inner_left.append(div_form_group);
+
+			var input_email = document.createElement("input");
+			input_email.className = "form_control";
+			input_email.type = "text";
+			input_email.placeholder="example@gmail.com"
+			input_email.value = "";
+			div_form_group.append(input_email);
+
+			var div_form_group2 = document.createElement("div");
+			div_form_group2.className = "form_group";
+			div_inner_left.append(div_form_group2);
+
+			var input_password = document.createElement("input");
+			input_password.className = "form_control";
+			input_password.type = "text";
+			input_password.placeholder="Password"
+			input_password.value = "";
+			div_form_group2.append(input_password);
+
+			var div_form_group3 = document.createElement("div");
+			div_form_group3.className = "form_group";
+			div_inner_left.append(div_form_group3);
+
+			var submit_button = document.createElement("input");
+			submit_button.value = "Login";
+			submit_button.type = "submit";
+			div_form_group3.append(submit_button);
+
+			var div_form_group4 = document.createElement("div");
+			div_form_group4.className = "form_group";
+			div_inner_left.append(div_form_group4);
+
+			var forgot_password = document.createElement("a");
+			var forgot_password_text = document.createTextNode("Forgot Password?");
+			forgot_password.className = "buttonForgotPassword";
+			forgot_password.href = "#";
+			forgot_password.append(forgot_password_text);
+			div_form_group4.append(forgot_password);
+
+			// create account form
+			var div_inner_right = document.createElement("div");
+			div_inner_right.className = "col-mid-6 login_form_right";
+			div_outer.append(div_inner_right);
+
+			var create_account_right_title = document.createElement("h3");
+			var create_account_right_title_text = document.createTextNode("Create Account");
+			create_account_right_title.append(create_account_right_title_text);
+			div_inner_right.append(create_account_right_title);
+
+			var div_form_group5 = document.createElement("div");
+			div_form_group5.className = "form_group";
+			div_inner_right.append(div_form_group5);
+
+			var create_email = document.createElement("input");
+			create_email.className = "form_control";
+			create_email.type = "text";
+			create_email.placeholder="example@gmail.com"
+			create_email.value = "";
+			div_form_group5.append(create_email);
+
+			var div_form_group6 = document.createElement("div");
+			div_form_group6.className = "form_group";
+			div_inner_right.append(div_form_group6);
+
+			var create_password = document.createElement("input");
+			create_password.className = "form_control";
+			create_password.type = "text";
+			create_password.placeholder="Password"
+			create_password.value = "";
+			div_form_group6.append(create_password);
+
+			var div_form_group7 = document.createElement("div");
+			div_form_group7.className = "form_group";
+			div_inner_right.append(div_form_group7);
+
+			var create_button = document.createElement("input");
+			create_button.value = "Create Account";
+			create_button.type = "submit";
+			div_form_group7.append(create_button);
+
+			var div_form_group8 = document.createElement("div");
+			div_form_group8.className = "form_group";
+			div_inner_right.append(div_form_group8);
+
+			var create_forgot_password = document.createElement("a");
+			var create_forgot_password_text = document.createTextNode("Forgot Password?");
+			create_forgot_password.className = "buttonForgotPassword";
+			create_forgot_password.href = "#";
+			create_forgot_password.append(create_forgot_password_text);
+			div_form_group8.append(create_forgot_password);
+
+			var attatch_body = document.getElementById("attatch_body");
+			attatch_body.append(background_of_login_create);
+			setTimeout(function(){
+			    removeOnForeignClick("background_of_login_create");
+			}, 500);
+
+		}
+
 });
 
 account_buttons_notifications.addEventListener("click", function show_hide_notifications() {
@@ -120,5 +257,46 @@ account_buttons_notifications.addEventListener("click", function show_hide_notif
 });
 
 account_buttons_notes.addEventListener("click", function show_hide_notes_history() {
+
 	console.log("notes history clicked\n");
+
 });
+
+function removeOnForeignClick(id) {
+
+	if (document.getElementById(id)) {
+
+		var element = document.getElementById(id);
+
+		const outsideClickListener = event => {
+			if(!element.contains(event.target)) {
+				while(element.firstChild) {
+					element.removeChild(element.firstChild);
+				}
+				element.remove();
+				// removeElementAndChildren(id);
+			}
+		}
+
+		const removeClickListener = () => {
+        	document.removeEventListener('click', outsideClickListener)
+    	}
+
+    	document.addEventListener('click', outsideClickListener)
+
+	}
+
+}
+
+// function removeElementAndChildren(id) {
+//
+// 	if (document.getElementById(id)) {
+// 		var element = document.getElementById(id);
+// 		while(element.firstChild) {
+// 			element.removeChild(element.firstChild);
+// 		}
+// 		element.remove();
+// 	}
+// 	return;
+//
+// }
